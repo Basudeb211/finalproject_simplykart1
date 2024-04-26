d8480a50cca61597b7ba71064b1c02cfe119117d
sibaprasad474 <83807278+sibaprasad474@users.noreply.github.com>
2024-04-26 12:51:43 +0530

    changed logo, removed dollars and other stuffs

--- includes/main.php
+++ includes/main.php
@@ -1,3 +1,15 @@
+<!-- <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+  </script> -->
 </head>
 
 <body>
